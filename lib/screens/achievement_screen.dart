7806a5d4414c03c3965de3a449ba432cf448a805
efe dalbay <dalbayefe68@gmail.com>
2025-01-29 06:47:35 +0300

    Başarılar Ekranı ve Widgetı
    Başarılar ekranımı tamamladım. Widgets klasöründe widgetı oluşturdum ve başarılar ekranına bağladım.

--- lib/screens/achievement_screen.dart
+++ lib/screens/achievement_screen.dart
@@ -4,6 +4,7 @@ import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import '../themes/themes.dart';
 import '../widgets/bottom_menu.dart';
+import '../widgets/trophy_widget.dart';
 
 class AchievementScreen extends StatelessWidget {
   const AchievementScreen({super.key});
@@ -14,7 +15,7 @@ class AchievementScreen extends StatelessWidget {
       appBar: AppBar(
         title: Center(
           child: Text(
-            'Ödüller',
+            'Başarılar',
             style: Theme.of(context).textTheme.headlineMedium,
           ),
         ),
@@ -42,7 +43,7 @@ class AchievementScreen extends StatelessWidget {
                   color: Theme.of(context).colorScheme.onPrimaryContainer,
                 ),
               ),
-              accountName: Text("Hoşgeldiniz"),
+              accountName: const Text("Hoşgeldiniz"),
               accountEmail: null,
               decoration: BoxDecoration(
                 color: Theme.of(context).colorScheme.primary,
@@ -103,8 +104,91 @@ class AchievementScreen extends StatelessWidget {
           ],
         ),
       ),
-      body:Center(
-        child: Text("Ödül ekranı"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surfaceVariant,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
+                ),
+                child: ListView(
+                  padding: const EdgeInsets.all(24),
+                  children: [
+                    TrophyWidget(
+                      icon: Icons.emoji_events,
+                      title: "İlk Gün",
+                      subtitle: "Tebrikler! İlk Günü Tamamladın.",
+                      onTap: () {},
+                    ),
+                    const SizedBox(height: 16),
+                    TrophyWidget(
+                      icon: Icons.currency_lira,
+                      title: "100 TL",
+                      subtitle: "Tebrikler! 100 Lira Biriktirdin.",
+                      onTap: () {},
+                    ),
+                    const SizedBox(height: 16),
+                    TrophyWidget(
+                      icon: Icons.emoji_events,
+                      title: "10 Gün",
+                      subtitle: "Tebrikler! 10 Gündür Sigarayı QuitZoneladın.",
+                      onTap: () {},
+                    ),
+                    const SizedBox(height: 16),
+                    TrophyWidget(
+                      icon: Icons.currency_lira,
+                      title: "1000TL",
+                      subtitle:
+                          "Tebrikler! 1000TL Hedeflerine Hızla İlerliyorsun.",
+                      onTap: () {
+                        context.go("/goals");
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    TrophyWidget(
+                      icon: Icons.emoji_events,
+                      title: "30 Gün",
+                      subtitle:
+                          "Tebrikler! 1 Ay Oldu Bile Bırakmayı Sakın Düşünme.",
+                      onTap: () {},
+                    ),
+                    const SizedBox(height: 16),
+                    TrophyWidget(
+                      icon: Icons.currency_lira,
+                      title: "2500 TL",
+                      subtitle: "Tebrikler! 2500 TL, Kaç Hedefine Ulaştın?",
+                      onTap: () {
+                        context.go("/goals");
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    TrophyWidget(
+                      icon: Icons.emoji_events,
+                      title: "45 Gün",
+                      subtitle:
+                          "Tebrikler! 45 Gün, Kendini Nasıl Hissediyorsun.",
+                      onTap: () {},
+                    ),
+                    const SizedBox(height: 16),
+                    TrophyWidget(
+                      icon: Icons.currency_lira,
+                      title: "5000 TL",
+                      subtitle: "Tebrikler! 5000 Tl,Yeni Ayakkabı? Yeni Ceket?",
+                      onTap: () {
+                        context.go("/goals");
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomMenu(),
     );
